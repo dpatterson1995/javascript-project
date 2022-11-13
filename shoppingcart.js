9fb5e42c9a858ddb969dad0ec61e655816b0e7f2
Jvail777 <111923863+Jvail777@users.noreply.github.com>
2022-11-13 18:00:42 -0500

    Revert "More shopping cart"

--- shoppingcart.js
+++ shoppingcart.js
@@ -1,3 +1,9 @@
+// let removeCartItemsButtons = document.getElementsByClassName("btn-danger");
+
+// for (let i = 0; i < removeCartItemsButtons.length; i++) {
+//   let button = removeCartItemsButtons[i];
+//   button.addEventListener("click", removeCartItem); 
+// }
 
 let quantityInputs = document.getElementsByClassName('cart-quantity-input');
 for (let i = 0; i < quantityInputs.length; i++){
@@ -5,12 +11,11 @@ for (let i = 0; i < quantityInputs.length; i++){
     input.addEventListener('change', quantityChanged);
 }
 
-let addToCartButtons = document.getElementsByClassName("add-to-cart-btn");
-for (let i = 0; i < addToCartButtons.length; i++) {
-  let button = addToCartButtons[i];
-  button.addEventListener('click', addToCartClicked)
-}
-
+// function removeCartItem(event){
+//     let buttonClicked = event.target;
+//     buttonClicked.parentElement.parentElement.remove();
+//     updateCartTotal();
+//   }
 
   function quantityChanged(event){
     let input = event.target
@@ -19,24 +24,6 @@ for (let i = 0; i < addToCartButtons.length; i++) {
     }
     updateCartTotal();
   }
-
-  // not working correctly yet
-function addToCartClicked(event){
-  let button = event.target;
-  let product = document.getElementsByClassName('product-category')[0].innerText;
-  let itemName = document.getElementsByClassName('card-title')[0].innerTexxt;
-  let itemPrice = document.getElementsByClassName('item-price')[0].innerText;
- 
-  addItemToCart(product, itemName, itemPrice);
-}
-
-function addItemToCart(product, itemName, itemPrice){
-  let cartRow = document.createElement('div');
-  let cartItems = document.getElementsByClassName('cart-items')[0];
-  let cartRowContents = product + itemName + itemPrice;
-  cartRow.innerText = cartRowContents;
-  cartItems.append(cartRow)
-}
 
 function updateCartTotal() {
   let cartItemContainer = document.getElementsByClassName("cart-items")[0];

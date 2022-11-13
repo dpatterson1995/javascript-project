f30d1af68fae05e2a7112350fbab7e993069cab0
Rhianon Hall <hallrhia@gmail.com>
2022-11-13 14:06:08 -0500

    updated shopping cart

--- shoppingcart.js
+++ shoppingcart.js
@@ -2,12 +2,28 @@ let removeCartItemsButtons = document.getElementsByClassName("btn-danger");
 console.log(removeCartItemsButtons);
 for (let i = 0; i < removeCartItemsButtons.length; i++) {
   let button = removeCartItemsButtons[i];
-  button.addEventListener("click", function (event) {
-    let buttonClicked = event.target;
-    buttonClicked.parentElement.remove();
-    updateCartTotal();
-  });
+  button.addEventListener("click", removeCartItem); 
 }
+
+let quantityInputs = document.getElementsByClassName('cart-quantity-input');
+for (let i = 0; i < quantityInputs.length; i++){
+    let input = quantityInputs[i];
+    input.addEventListener('change', quantityChanged);
+}
+
+function removeCartItem(event){
+    let buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal();
+  }
+
+  function quantityChanged(event){
+    let input = event.target
+    if (isNaN(input.value) || input.value <+ 0){
+        input.value = 1;
+    }
+    updateCartTotal();
+  }
 
 function updateCartTotal() {
   let cartItemContainer = document.getElementsByClassName("cart-items")[0];
@@ -16,15 +32,13 @@ function updateCartTotal() {
   for (let i = 0; i < cartRows.length; i++) {
     let cartRow = cartRows[i];
     let priceElement = cartRow.getElementsByClassName("cart-price")[0];
-    let quantityElement = cartRow.getElementsByClassName(
-      "cart-quantity-input"
-    )[0];
+    let quantityElement = cartRow.getElementsByClassName("cart-quantity-input")[0];
 
     let price = parseFloat(priceElement.innerText.replace("$", ""));
 
     let quantity = quantityElement.value;
     total = total + price * quantity;
   }
-  document.getElementsByClassName("cart-total-price")[0].innerText =
-    "$" + total;
+  total = Math.round(total * 100) / 100;
+  document.getElementsByClassName("cart-total-price")[0].innerText = "$" + total;
 }

24efa7d5d5277187867cf8f33f7696f316f91864
Veronika Jones <veronikashapiro@yahoo.com>
2022-11-16 03:34:20 -0500

    added shopping cart content to shopping cart

---
+++ products.js
@@ -0,0 +1,83 @@
+let products=[
+    { 
+     name: 'Bread', 
+     category: 'Pantry', 
+     price: 1.99, 
+     tag:'Bread.jpg',
+     salesTax: false, 
+     inCart: 0
+ },
+    { 
+     name: 'Dog Food', 
+     category: 'Pet', 
+     price: 45.00,
+     tag:'DOg.jpg', 
+     salesTax: true, 
+     inCart: 0
+ },
+    { 
+     name: 'Ground Pork', 
+     category: 'Meat', 
+     price: 14.99,
+     tag:'Beeef.jpg', 
+     salesTax: false, 
+     inCart: 0
+ },
+    { 
+     name: 'Milk', 
+     category: 'Dairy', 
+     price: 2.99, 
+     tag:'ilk.jpg',
+     salesTax: false, 
+     inCart: 0
+ },
+ {
+     name: 'Eggs',
+     category: 'Protein',
+     price: 2.59,
+     tag:'Eggs.jpg',
+     salesTax: false,
+     inCart: 0
+ },
+ {
+     name: 'Bananas',
+     category: 'Produce',
+     price: 3.79,
+     tag: 'Bana.jpg',
+     salesTax: false,
+     inCart: 0
+ },
+ {
+     name: 'Sushi',
+     category: 'Protein',
+     price: 8.99,
+     tag:'Sush.jpg',
+     salesTax: false,
+     inCart: 0
+ },
+ {
+     name: 'Yogurt',
+     category: 'Dairy',
+     price: 1.06,
+     tag:'Yop.webp',
+     salesTax: false,
+     inCart: 0
+ },
+ {
+     name: 'Tofu',
+     category:'Protein',
+     price: 2.48,
+     tag:'tofu_image.png',
+     salesTax: false,
+     inCart: 0
+ },
+ {
+     name: 'Carrots',
+     category: 'Produce',
+     price: 0.79,
+     tag:'carrots_image.png',
+     salesTax: false,
+     inCart: 0
+ }
+ 
+ ]
